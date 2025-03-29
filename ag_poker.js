1d3906be6938ed36c2ad76d391c0b040ddf51e1c
James <jbnguyen7@yahoo.com>
2025-03-29 15:23:16 -0400

    Session 14.3

--- ag_poker.js
+++ ag_poker.js
@@ -53,6 +53,7 @@ function playDrawPoker() {
    // Enable the Draw and Stand buttons after the deal
    dealButton.addEventListener("click", function() {
       if (pokerGame.currentBank >= pokerGame.currentBet) {
+         handValueText.textContent = "";
          disableObj(dealButton);
          disableObj(betSelection);
          enableObj(drawButton);
@@ -105,12 +106,24 @@ function playDrawPoker() {
          }
          cardImages[i].onclick = null;
       }
+
+      // Evaluate the hand drawn by user
+      handValueText.textContent = myHand.handType();
+
+      // Pay off the final hand
+      bankBox.value = pokerGame.payout(myHand.handOdds());
    });
    standButton.addEventListener("click", function() {
       enableObj(dealButton);
       enableObj(betSelection);
       disableObj(drawButton);
       disableObj(standButton);
+
+      // Evaluate the hand dealt to the user
+      handValueText.textContent = myHand.handType();
+
+      // Pay off the final hand
+      bankBox.value = pokerGame.payout(myHand.handOdds());
    });
 
    // Disable Poker Button
